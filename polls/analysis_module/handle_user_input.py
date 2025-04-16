b1cf1cab9df93e5541e120bbfeb529c053d1edcb
Mars <blackball1998@outlook.com>
2025-04-16 23:02:17 +0100

    choose the features and the target columns

--- polls/analysis_module/handle_user_input.py
+++ polls/analysis_module/handle_user_input.py
@@ -1,16 +1,18 @@
+import numpy as np
 import pandas as pd
 import statsmodels.api as sm
-import matplotlib.pyplot as plt
-import numpy as np
 
-def analysis_module(option, file):
+def handle_user_input(file):
+    df = pd.read_csv(file)
+    return df
+
+def analysis_module(option, file, features, target):
     print(option)
     if file:
         if option == 'regression':
             df = pd.read_csv(file)
-            print(df.head())
-            x = sm.add_constant(df[['pH']])
-            y = df['residual sugar']
+            x = sm.add_constant(df[features])
+            y = df[target]
             model = sm.OLS(y, x).fit()
             influence = model.get_influence()
             cooks_d, _ = influence.cooks_distance
@@ -19,6 +21,7 @@ def analysis_module(option, file):
             print("Influential points (potential outliers):", influential_points)
             percentage = len(influential_points) / len(df) * 100
             print("Percentage of influential points:", percentage)
+            # TODO: Which feature should be returned?
             data = df[['pH', 'total sulfur dioxide']].values.tolist()
             return data, percentage
         elif option == 'classification':
