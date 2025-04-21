f8592155c269df04bce76eb4587a30ea567c1ea4
Mitch Jones <mitch.jones0508@gmail.com>
2025-04-21 15:05:21 +0100

    analysis module classification rules

--- polls/analysis_module/handle_user_input.py
+++ polls/analysis_module/handle_user_input.py
@@ -6,7 +6,9 @@ import statsmodels.api as sm
 def analysis_module(option, file, features, target):
     df = pd.read_csv(file)
     result = {}
-    data_input = features + [target]
+    # if len(features) > 0:
+    #     data_input = features + [target]
+    # data_input = target
     if option == 'regression':
         x = sm.add_constant(df[features])
         y = df[target]
@@ -23,10 +25,12 @@ def analysis_module(option, file, features, target):
         return data, percentage
     elif option == 'classification':
         class_counter = df[target].value_counts().to_dict()
+        print(class_counter)
         if len(class_counter) == 2:
             max_key = max(class_counter.values())
             min_key = min(class_counter.values())
             result['imbalance_ratio'] = max_key / min_key
+            print(result['imbalance_ratio'])
         else:
             # TODO: imbalance degree
             print('multiclass classification')
