2069e28bf84fb72f61f23737e053243a45351719
Mars <blackball1998@outlook.com>
2025-04-22 16:23:22 +0100

    fix the 2D/3D chart

--- polls/views.py
+++ polls/views.py
@@ -13,13 +13,8 @@ def home(request):
     features = query.getlist("features")
     target = query.get("target")
     if task:
-        (data, result) = analysis_module(task, file, features, target)
+        data, result = analysis_module(task, file, features, target)
         print(data)
-        if task == 'clustering' or 'regression':
-            if len(data[0]) == 3:
-                data_min = [min(sublist[0] for sublist in data for item in sublist),min(sublist[1] for sublist in data for item in sublist),min(sublist[2] for sublist in data for item in sublist)]
-                data_max = [max(sublist[0] for sublist in data for item in sublist),max(sublist[1] for sublist in data for item in sublist),max(sublist[2] for sublist in data for item in sublist)]
-                data = [data,data_min,data_max]
         return render(request, f"{task}.html", {"result": result, "data": data})
     return render(request, "home.html")
 
